e22604683e440456dc97723dc833b872a1d6757a
yunusekr <yunus_eker94@hotmail.com>
2025-06-13 23:59:55 +0300

    update text fonts

--- ekrcoaching/src/components/Navbar.jsx
+++ ekrcoaching/src/components/Navbar.jsx
@@ -106,7 +106,7 @@ const Navbar = () => {
       </div>
 
       {isOpen && (
-        <div className="md:hidden px-4 pb-4 text-gray-700 text-sm bg-gray-50 rounded-b shadow-inner text-[1.2rem] ">
+        <div className="md:hidden px-4 pb-4 text-gray-700 text-base bg-gray-50 rounded-b shadow-inner ">
           <ul className="space-y-1 font-semibold text-black">
             <li>
               <a
